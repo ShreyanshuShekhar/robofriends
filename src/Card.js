cd6c3e51aa4d896130d08c6777e1a7f48bea5cdb
ShreyanshuShekhar <Shreyanshushekhar.ss@gmail.com>
2018-12-28 16:30:12 +0530

    Added robohash API.

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Card = () => {
+  return(
+    <div className='bg-orange dib br3 pa3 ma2 grow bw2 shadow-5' >
+      <img alt='photo' src='https://robohash.org/test?200×200'/>
+      <div>
+        <h2>Sonal Shekhar</h2>
+        <p>shreyanshushekhar.ss@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Card from './Card';
+import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
+
+ReactDOM.render(
+  <div>
+  <Card />
+  <Card />
+  <Card />
+  </div>
+  , document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
